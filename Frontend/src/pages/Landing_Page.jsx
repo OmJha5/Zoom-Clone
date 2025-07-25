091c4900968f5fc8afb30c5f7b0119bb0eedc051
om <omjha003@gmail.com>
2025-07-25 18:03:18 +0530

    Done with landing Page frontend design

--- Frontend/src/pages/Landing_Page.jsx
+++ Frontend/src/pages/Landing_Page.jsx
@@ -1,9 +1,44 @@
 import React from 'react'
+import "../App.css";
+import { Link } from 'react-router-dom';
 
 export default function Landing_Page() {
   return (
-    <div>
-      Landing Page
+    <div className='landingPageContainer'>
+      <nav>
+        <div className="navHeader">
+            <h2>
+                <Link style={{textDecoration : "none" , color : "white"}} to={"/"}>Zoom Video Call</Link>
+            </h2>
+        </div>
+        <div className="navList">
+            <ul>
+                <li>Join As Guest</li>
+                <li>Register</li>
+                <li>
+                    <div role="button">
+                        <p>Login</p>
+                    </div>
+                </li>
+            </ul>
+
+        </div>
+      </nav>
+
+      <main className="landingPageMainContainer">
+        <div>
+            <h1><span style={{color : "#FF9839"}}>Connect</span> with your old ones</h1>
+            <p className='mainP'>Join a video call with your friends and family, no matter where they are.</p>
+
+            <div role="button">
+                <Link to={"/home"}>Get Started</Link>
+            </div>
+        </div>
+
+        <div>
+            <img src="images/mobile.png" alt="" />
+        </div>
+      </main>
     </div>
   )
 }
