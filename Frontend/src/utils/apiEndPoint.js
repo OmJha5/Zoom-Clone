3c31de7d942ac5c43298ba72ddee1fde5ba894bb
om <omjha003@gmail.com>
2025-08-02 05:56:08 +0530

    Fixed a bug

--- Frontend/src/utils/apiEndPoint.js
+++ Frontend/src/utils/apiEndPoint.js
@@ -2,6 +2,6 @@
 
 export let USER_ENDPOINT_API = "https://zoom-clone-backend-ebuh.onrender.com/api/users"
 
-// export let server_url = "http://localhost:8080"
+// export let backend_url = "http://localhost:8080"
 
-export let server_url = "https://zoom-clone-backend-ebuh.onrender.com"
+export let backend_url = "https://zoom-clone-backend-ebuh.onrender.com"
